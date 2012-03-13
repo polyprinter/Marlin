8763e6cb58ce639a2dbcfec1155f49946a84a5a4
lincomatic <lincomatic@hotmail.com>
2012-03-13 12:42:19 -0700

    added printrboard and updated pins for Teensylu

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -827,35 +827,35 @@
 #define KNOWN_BOARD 1
 
 
-#define X_STEP_PIN          0  
-#define X_DIR_PIN           1  
-#define X_ENABLE_PIN       39 
-#define X_MIN_PIN          13 
+#define X_STEP_PIN         28
+#define X_DIR_PIN          29
+#define X_ENABLE_PIN       19
+#define X_MIN_PIN          25
 #define X_MAX_PIN          -1    
 
-#define Y_STEP_PIN          2  
-#define Y_DIR_PIN           3 
-#define Y_ENABLE_PIN       38 
-#define Y_MIN_PIN          14 
+#define Y_STEP_PIN         30
+#define Y_DIR_PIN          31
+#define Y_ENABLE_PIN       26
+#define Y_MIN_PIN           20
 #define Y_MAX_PIN          -1    
 
-#define Z_STEP_PIN          4
-#define Z_DIR_PIN           5 
-#define Z_ENABLE_PIN       23 
-#define Z_MIN_PIN          15 
+#define Z_STEP_PIN         32
+#define Z_DIR_PIN          33
+#define Z_ENABLE_PIN       17
+#define Z_MIN_PIN          27
 #define Z_MAX_PIN          -1    
 
-#define E0_STEP_PIN         6  
-#define E0_DIR_PIN          7 
-#define E0_ENABLE_PIN       19 
+#define E0_STEP_PIN         34
+#define E0_DIR_PIN          35
+#define E0_ENABLE_PIN       13
 
 
 
-#define HEATER_0_PIN       21  // Extruder
+#define HEATER_0_PIN       15  // Extruder
 #define HEATER_1_PIN       -1
 #define HEATER_2_PIN       -1
-#define HEATER_BED_PIN     20  // Bed
-#define FAN_PIN            22  // Fan   
+#define HEATER_BED_PIN     14  // Bed
+#define FAN_PIN            16  // Fan   
 
 #define TEMP_0_PIN          7  // Extruder
 #define TEMP_1_PIN         -1
@@ -863,7 +863,7 @@
 #define TEMP_BED_PIN        6  // Bed
 
 #define SDPOWER            -1
-#define SDSS                8
+#define SDSS                20
 #define LED_PIN            -1
 #define PS_ON_PIN          -1
 #define KILL_PIN           -1 
@@ -871,9 +871,65 @@
 
 #ifndef SDSUPPORT
 // these pins are defined in the SD library if building with SD support  
-  #define SCK_PIN           9 
-  #define MISO_PIN         11 
-  #define MOSI_PIN         10 
+  #define SCK_PIN          21
+  #define MISO_PIN         22 
+  #define MOSI_PIN         23 
+#endif
+#endif
+
+/****************************************************************************************
+* Printrboard Rev. B pin assingments (ATMEGA90USB1286)
+* Requires the Teensyduino software with Teensy2.0++ selected in arduino IDE!
+* See http://reprap.org/wiki/Printrboard for more info
+****************************************************************************************/
+#if MOTHERBOARD == 81
+#define MOTHERBOARD 81
+#define KNOWN_BOARD 1
+
+#define X_STEP_PIN         28
+#define X_DIR_PIN          29
+#define X_ENABLE_PIN       19
+#define X_MIN_PIN          47
+#define X_MAX_PIN          -1
+
+#define Y_STEP_PIN         30
+#define Y_DIR_PIN          31
+#define Y_ENABLE_PIN       18
+#define Y_MIN_PIN           20
+#define Y_MAX_PIN          -1
+
+#define Z_STEP_PIN         32
+#define Z_DIR_PIN          33
+#define Z_ENABLE_PIN       17
+#define Z_MIN_PIN          36
+#define Z_MAX_PIN          -1
+
+#define E0_STEP_PIN         34
+#define E0_DIR_PIN          35
+#define E0_ENABLE_PIN       13
+
+#define HEATER_0_PIN       15  // Extruder
+#define HEATER_1_PIN       -1
+#define HEATER_2_PIN       -1
+#define HEATER_BED_PIN     14  // Bed
+#define FAN_PIN            16  // Fan
+
+#define TEMP_0_PIN          1  // Extruder
+#define TEMP_1_PIN         -1
+#define TEMP_2_PIN         -1
+#define TEMP_BED_PIN        0  // Bed
+
+#define SDPOWER            -1
+#define SDSS                2
+#define LED_PIN            -1
+#define PS_ON_PIN          -1
+#define KILL_PIN           -1
+
+#ifndef SDSUPPORT
+// these pins are defined in the SD library if building with SD support
+  #define SCK_PIN          21
+  #define MISO_PIN         22
+  #define MOSI_PIN         23
 #endif
 #endif
 
